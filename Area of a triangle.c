4696d7d88f0a915ecc3f55320883a966ab7d0ca6
mumarasif <93067191+mumarasif@users.noreply.github.com>
2021-10-25 09:50:10 +0500

    Add files via upload

---
+++ Area of a triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    float c;
+    printf("Enter base of triangle:");
+    scanf("%d",&a);
+    printf("Enter height of triangle:");
+    scanf("%d",&b);
+    c=1/2*(a*b);
+    printf("The Area of triangle is:%d",&c);
+    return 0;
+
+
+}
